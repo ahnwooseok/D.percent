07078c11f9ff9044d617386f5a227d64f610ce18
ahnwooseok <peterdntjr@naver.com>
2022-02-21 01:34:45 +0900

    recruitment title width 56%
    ok

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d921855abeb35ff"; }
+/******/ 		__webpack_require__.h = function() { return "7e6ad32b432edf53"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
